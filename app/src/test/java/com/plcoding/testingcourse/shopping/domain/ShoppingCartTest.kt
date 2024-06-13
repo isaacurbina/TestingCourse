d4accf44da7db013fe1a5d55ab0947645cccd424
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-06-12 19:10:34 -0500

    3.7 Adding test for ShoppingCart

---
+++ app/src/test/java/com/plcoding/testingcourse/shopping/domain/ShoppingCartTest.kt
@@ -0,0 +1,44 @@
+package com.plcoding.testingcourse.shopping.domain
+
+import assertk.assertFailure
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class ShoppingCartTest {
+
+    private lateinit var cart: ShoppingCart
+
+    @BeforeEach
+    fun setUp() {
+        cart = ShoppingCart()
+    }
+
+    @Test
+    fun `Add multiple products, total price sum is correct`() {
+        val product = Product(
+            id = 0,
+            name = "Ice cream",
+            price = 5.0
+        )
+        cart.addProduct(product, 4)
+
+        val priceSum = cart.getTotalCost()
+
+        assertThat(priceSum).isEqualTo(20.0)
+    }
+
+    @Test
+    fun `Add product with negative quantity, throws Exception`() {
+        val product = Product(
+            id = 0,
+            name = "Ice cream",
+            price = 5.0
+        )
+
+        assertFailure {
+            cart.addProduct(product, -5)
+        }
+    }
+}
