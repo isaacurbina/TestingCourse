688c06f8d2ad33bd5420e97c21af59462889fb69
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-06-20 17:43:24 -0500

    7.32 Testing launched coroutines

---
+++ app/src/test/java/com/plcoding/testingcourse/part7/presentation/ProfileViewModelTest.kt
@@ -0,0 +1,56 @@
+@file:OptIn(ExperimentalCoroutinesApi::class)
+
+package com.plcoding.testingcourse.part7.presentation
+
+import androidx.lifecycle.SavedStateHandle
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import assertk.assertions.isFalse
+import assertk.assertions.isNull
+import com.plcoding.testingcourse.util.MainCoroutineExtension
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+@ExtendWith(MainCoroutineExtension::class)
+internal class ProfileViewModelTest {
+
+    private lateinit var viewModel: ProfileViewModel
+    private lateinit var repository: UserRepositoryFake
+
+    @BeforeEach
+    fun setUp() {
+        repository = UserRepositoryFake()
+        viewModel = ProfileViewModel(
+            repository,
+            SavedStateHandle(
+                initialState = mapOf(
+                    "userId" to repository.profileToReturn.user.id
+                )
+            )
+        )
+    }
+
+    @Test
+    fun `Test loading profile success`() = runTest {
+        viewModel.loadProfile()
+        advanceUntilIdle()
+
+        assertThat(viewModel.state.value.profile).isEqualTo(repository.profileToReturn)
+        assertThat(viewModel.state.value.isLoading).isFalse()
+    }
+
+    @Test
+    fun `Test loading profile error`() = runTest {
+        repository.errorToReturn = Exception("Test exception")
+        viewModel.loadProfile()
+        advanceUntilIdle()
+
+        assertThat(viewModel.state.value.profile).isNull()
+        assertThat(viewModel.state.value.errorMessage).isEqualTo("Test exception")
+        assertThat(viewModel.state.value.isLoading).isFalse()
+    }
+}
