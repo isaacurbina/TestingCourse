043f7e977532e266871a3d93101b35f21bf17db1
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-07-01 18:17:13 -0500

    11.14 Testing permission requests (#18)

--- app/src/main/java/com/plcoding/testingcourse/MainActivity.kt
+++ app/src/main/java/com/plcoding/testingcourse/MainActivity.kt
@@ -1,18 +1,33 @@
 package com.plcoding.testingcourse
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.core.app.ActivityCompat
 import com.plcoding.testingcourse.ui.theme.TestingCourseTheme
 
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,25 +37,50 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    var showPermissionError by rememberSaveable {
+                        mutableStateOf(false)
+                    }
+                    // PERMISSION REQUEST
+                    val permissionLauncher = rememberLauncherForActivityResult(
+                        contract = ActivityResultContracts.RequestPermission(),
+                        onResult = { wasGranted ->
+                            if (!wasGranted && !shouldShowRequestPermissionRationale(
+                                    android.Manifest.permission.RECORD_AUDIO
+                                )
+                            ) {
+                                showPermissionError = true
+                            }
+                        }
+                    )
+
+                    Button(onClick = {
+                        val hasPermission = ActivityCompat.checkSelfPermission(
+                            applicationContext,
+                            android.Manifest.permission.RECORD_AUDIO
+                        ) == PackageManager.PERMISSION_GRANTED
+
+                        if (hasPermission) {
+                            // Start recording...
+                        } else {
+                            permissionLauncher.launch(android.Manifest.permission.RECORD_AUDIO)
+                        }
+                    }) {
+                        Text(text = "Record")
+                    }
+                    if (showPermissionError) {
+                        Dialog(onDismissRequest = {
+                            showPermissionError = false
+                        }) {
+                            Text(
+                                text = "Can't record without permission",
+                                modifier = Modifier
+                                    .background(Color.White, RoundedCornerShape(100))
+                                    .padding(16.dp)
+                            )
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TestingCourseTheme {
-        Greeting("Android")
     }
 }
